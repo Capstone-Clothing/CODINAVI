89f3feb2222df73bf7d6ad5424b19f82a9764598
JMJ <ghtntkdnsk@naver.com>
2024-02-17 21:51:08 +0900

    add SearchOtherLocation

--- app/src/main/java/com/example/capstonecodinavi/WeatherActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/WeatherActivity.kt
@@ -10,8 +10,6 @@ import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
-import android.os.Build
-import android.provider.Telephony.Mms.Addr
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -91,11 +89,11 @@ class WeatherActivity : AppCompatActivity() {
                 success?.let { location ->
                     lat = location.latitude
                     lon = location.longitude
-//                    lat = 37.3401906
-//                    lon = 126.7335293
+//                    lat = 35.2070205
+//                    lon = 129.1025573
                     getCurrentAddress(lat!!, lon!!)
-                    getCurrentWeather(lat!!, lon!!)
                     getCurrentSeason()
+                    getCurrentWeather(lat!!, lon!!)
                 }
             }
     }
@@ -135,22 +133,21 @@ class WeatherActivity : AppCompatActivity() {
         }
 
     }
-    fun getCurrentWeather(lat: Double, lon: Double): String? {
-        var weatherStr: String? = null
+    fun getCurrentWeather(lat: Double, lon: Double) {
         val url = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2d360c1fe9d2bade8fc08a1679683e24"
+        Log.d("check12345","$lat, $lon")
         val request = object :
             StringRequest(
                 Method.GET,
                 url,
                 Response.Listener { response ->
                     try {
-                        // JSON 데이터 가져오기
                         val jsonObject = JSONObject(response)
                         val weather = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main")
                         val kelvin = jsonObject.getJSONObject("main").getString("temp").toDouble()
-                        val celsius = changeKelvinToCelsius(kelvin)
+                        var celsius = changeKelvinToCelsius(kelvin)
+                        var weatherStr: String
 
-                        //날씨
                         if (weather.contains("Rain")) {
                             weatherStr = "비"
                         } else if (weather.contains("Snow")) {
@@ -162,6 +159,7 @@ class WeatherActivity : AppCompatActivity() {
                         }
 
                         binding.currentWeatherTv.text = "계절은 ${season}, 날씨는 ${weatherStr}, 기온은 ${celsius}도 입니다."
+                        recommendCodi(celsius.toDouble())
                     } catch (e: JSONException) {
                         e.printStackTrace()
                     }
@@ -170,7 +168,6 @@ class WeatherActivity : AppCompatActivity() {
             ) { }
         request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
         requestQueue!!.add(request)
-        return weatherStr
     }
     fun getCurrentSeason() {
         val now = System.currentTimeMillis()
@@ -190,6 +187,32 @@ class WeatherActivity : AppCompatActivity() {
         val df = DecimalFormat("#.#")
         df.roundingMode = RoundingMode.DOWN
         return df.format(changedTemp)
+    }
+    private fun recommendCodi(temp: Double) {
+        if (temp >= 28) {
+            binding.recommendClothTv.text = "상의는 민소매, 반팔을 추천해드리며 하의는 짧은 치마, 린넨 옷, 반바지를 추천드립니다."
+        }
+        else if (temp in 23.0..27.0) {
+            binding.recommendClothTv.text = "상의는 얇은 셔츠, 반팔을 추천해드리며 하의는 반바지, 면바지를 추천드립니다."
+        }
+        else if (temp in 20.0..22.0) {
+            binding.recommendClothTv.text = "상의는 블라우스, 긴팔티를 추천해드리며 하의는 면바지, 슬랙스를 추천드립니다."
+        }
+        else if (temp in 17.0..19.0) {
+            binding.recommendClothTv.text = "상의는 얇은 가디건, 니트, 맨투맨, 후드를 추천해드리며 하의는 긴 바지를 추천드립니다."
+        }
+        else if (temp in 12.0..16.0) {
+            binding.recommendClothTv.text = "상의는 자켓 또는 청자켓, 가디건, 니트를 추천해드리며 하의는 스타킹, 청바지를 추천드립니다."
+        }
+        else if (temp in 9.0..11.0) {
+            binding.recommendClothTv.text = "상의는 트렌치 코트, 야상, 점퍼를 추천해드리며 하의는 스타킹, 기모바지를 추천드립니다."
+        }
+        else if (temp in 5.0..8.0) {
+            binding.recommendClothTv.text = "상의는 울 코트 또는 가죽 옷과 히트텍을 추천해드리며 하의 또한 기모가 들어간 바지를 추천드립니다."
+        }
+        else {
+            binding.recommendClothTv.text = "상의는 패딩, 두꺼운 코트, 누빔 옷, 기모가 들어간 소재, 그리고 목도리를 걸치시는 것을 추천드립니다."
+        }
     }
     override fun onRequestPermissionsResult(
         requestCode: Int,
