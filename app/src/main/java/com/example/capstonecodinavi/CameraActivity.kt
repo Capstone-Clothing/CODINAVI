9c4e14dd63cb2e271d327f6d909f807675de00c5
JMJ <ghtntkdnsk@naver.com>
2024-02-17 22:52:04 +0900

    modify camera

--- app/src/main/java/com/example/capstonecodinavi/CameraActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/CameraActivity.kt
@@ -13,14 +13,13 @@ import com.example.capstonecodinavi.databinding.ActivityCameraBinding
 import android.Manifest
 
 class CameraActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCameraBinding
+    lateinit var binding: ActivityCameraBinding
     val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        checkPermission()
+        binding.clothIv.setImageBitmap(MainActivity.imageBitmap)
         action()
     }
 
@@ -34,15 +33,6 @@ class CameraActivity : AppCompatActivity() {
             val intent = Intent(this, UserActivity::class.java)
             startActivity(intent)
         }
-
-        binding.mainBtnCameraOpen.setOnClickListener {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                takePictureIntent.resolveActivity(packageManager)?.also {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
-        }
-
         // 추가해야 할 것 : 밑의 두 개의 콜백 함수가 실행되기 위해서는 먼저 clothIv에 이미지가 있어야 함. -> if문으로 조건 걸어야 함.
         binding.codiBtn.setOnClickListener {
             val intent = Intent(this, CodiActivity::class.java)
@@ -52,41 +42,6 @@ class CameraActivity : AppCompatActivity() {
         binding.colorBtn.setOnClickListener {
             val intent = Intent(this, ColorActivity::class.java)
             startActivity(intent)
-        }
-    }
-    private fun checkPermission() {
-        var permission = mutableMapOf<String, String>()
-        permission["camera"] = Manifest.permission.CAMERA
-
-        var denied = permission.count { ContextCompat.checkSelfPermission(this, it.value) == PackageManager.PERMISSION_DENIED}
-
-        if(denied > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(permission.values.toTypedArray(), REQUEST_IMAGE_CAPTURE)
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == REQUEST_IMAGE_CAPTURE) {
-            var count = grantResults.count { it == PackageManager.PERMISSION_DENIED }
-
-            if(count!=0) {
-                Toast.makeText(applicationContext, "권한을 동의해주세요", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-
-            binding.clothIv.setImageBitmap(imageBitmap)
         }
     }
 }
