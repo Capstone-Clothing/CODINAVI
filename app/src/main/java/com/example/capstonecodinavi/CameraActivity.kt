154f3c4754b872e0d4ca3222afce5d80773445cd
JMJ <ghtntkdnsk@naver.com>
2024-03-10 23:24:04 +0900

    refactoring

--- app/src/main/java/com/example/capstonecodinavi/CameraActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/CameraActivity.kt
@@ -21,19 +21,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.core.Preview
 import androidx.camera.core.CameraSelector
 import android.util.Log
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.video.FallbackStrategy
-import androidx.camera.video.MediaStoreOutputOptions
-import androidx.camera.video.Quality
-import androidx.camera.video.QualitySelector
-import androidx.camera.video.VideoRecordEvent
-import androidx.core.content.PermissionChecker
 import com.example.capstonecodinavi.databinding.ActivityCameraBinding
-import java.nio.ByteBuffer
-import java.text.SimpleDateFormat
-import java.util.Locale
-
 typealias LumaListener = (luma: Double) -> Unit
 
 class CameraActivity : AppCompatActivity() {
