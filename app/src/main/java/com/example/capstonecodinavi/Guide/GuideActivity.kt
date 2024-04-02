2e089920bed38ca232126ebb3d7477be5b0fd5fa
yoonjin301 <maruru301@naver.com>
2024-04-02 17:35:01 +0900

    Gender BottomBar

--- app/src/main/java/com/example/capstonecodinavi/Guide/GuideActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Guide/GuideActivity.kt
@@ -18,16 +18,6 @@ class GuideActivity : AppCompatActivity() {
     }
 
     private fun action() {
-        binding.homeBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.profileBtn.setOnClickListener {
-            val intent = Intent(this, UserActivity::class.java)
-            startActivity(intent)
-        }
-
         binding.introduceCameraBtn.setOnClickListener {
             val intent = Intent(this, IntroduceCameraBtn::class.java)
             startActivity(intent)
