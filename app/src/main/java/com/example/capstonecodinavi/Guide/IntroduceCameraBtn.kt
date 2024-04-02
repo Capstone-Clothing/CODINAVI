d2f3b42c45e77a1a353b014dd8de55c20967274d
yoonjin301 <maruru301@naver.com>
2024-04-02 17:44:06 +0900

    feat : IntroduceCamera BottomBar

--- app/src/main/java/com/example/capstonecodinavi/Guide/IntroduceCameraBtn.kt
+++ app/src/main/java/com/example/capstonecodinavi/Guide/IntroduceCameraBtn.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.capstonecodinavi.Main.MainActivity
+import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivityIntroduceCameraBtnBinding
 
@@ -18,14 +19,20 @@ class IntroduceCameraBtn : AppCompatActivity() {
     }
 
     private fun action() {
-        binding.homeBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.profileBtn.setOnClickListener {
-            val intent = Intent(this, UserActivity::class.java)
-            startActivity(intent)
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
