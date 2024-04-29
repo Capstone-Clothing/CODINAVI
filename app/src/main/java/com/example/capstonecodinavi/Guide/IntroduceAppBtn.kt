8eae733449e820dc801e5366169b834815dab84d
로인 <aroyne@naver.com>
2024-04-29 21:49:14 +0900

    add : introduceAppBtn page

---
+++ app/src/main/java/com/example/capstonecodinavi/Guide/IntroduceAppBtn.kt
@@ -0,0 +1,38 @@
+package com.example.capstonecodinavi.Guide
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonecodinavi.Main.MainActivity
+import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.User.UserActivity
+import com.example.capstonecodinavi.databinding.ActivityIntroduceAppBtnBinding
+
+class IntroduceAppBtn : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroduceAppBtnBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroduceAppBtnBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle(" ")
+        action()
+    }
+
+    private fun action() {
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
