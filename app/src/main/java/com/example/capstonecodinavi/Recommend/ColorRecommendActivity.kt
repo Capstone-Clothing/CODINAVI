08ea23837d431a81af4e5fe16af60a75985c6545
JMJ <ghtntkdnsk@naver.com>
2024-05-08 22:57:17 +0900

    connect ColorRecommend with our server

--- app/src/main/java/com/example/capstonecodinavi/Recommend/ColorRecommendActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/ColorRecommendActivity.kt
@@ -3,19 +3,38 @@ package com.example.capstonecodinavi.Recommend
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivityColorBinding
+import org.json.JSONException
+import org.json.JSONObject
 
 class ColorRecommendActivity : AppCompatActivity() {
     private lateinit var binding: ActivityColorBinding
+    private lateinit var color: String
+
+    companion object {
+        var requestQueue: RequestQueue? = null
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityColorBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setTitle(" ")
+        initData()
         action()
+        color = "파란색"
+        recommendColor(color)
+    }
+    private fun initData() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(applicationContext)
+        }
     }
 
     private fun action() {
@@ -42,6 +61,21 @@ class ColorRecommendActivity : AppCompatActivity() {
             }
         }
     }
+    private fun recommendColor(color: String) {
+        val url = "http://3.34.34.170:8080/color/recommend?color=${color}"
+        val request = object : StringRequest(Method.GET, url, Response.Listener { response ->
+            try {
+                val jsonObject = JSONObject(response)
+                val colorInfo = jsonObject.getString("matchColor")
+                binding.colorTv.text = colorInfo
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+        }, Response.ErrorListener { }) {}
+        request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
+        requestQueue!!.add(request)
+    }
 }
 
-//test
