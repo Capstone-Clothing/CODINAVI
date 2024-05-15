d316db62ce98f7e433aa83063c62167850a0961b
yoonjin301 <maruru301@naver.com>
2024-05-16 05:04:55 +0900

    Add weatherTVBtn

--- app/src/main/java/com/example/capstonecodinavi/Guide/IntroduceWeatherBtn.kt
+++ app/src/main/java/com/example/capstonecodinavi/Guide/IntroduceWeatherBtn.kt
@@ -3,9 +3,14 @@ package com.example.capstonecodinavi.Guide
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
+import com.example.capstonecodinavi.Camera.CameraActivity
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
+import com.example.capstonecodinavi.Weather.WeatherActivity
 import com.example.capstonecodinavi.databinding.ActivityIntroduceWeatherBtnBinding
 
 class IntroduceWeatherBtn : AppCompatActivity() {
@@ -34,5 +39,22 @@ class IntroduceWeatherBtn : AppCompatActivity() {
                 else -> false
             }
         }
+
+        val textView = findViewById<TextView>(R.id.weatherTVBtn) // XML에서 정의한 TextView를 가져옵니다.
+        val content = textView.text.toString() // TextView의 문자열 내용을 가져옵니다.
+        val spannableString = SpannableString(content) // SpannableString을 생성합니다.
+        spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, 0) // SpannableString에 밑줄을 추가합니다.
+        textView.text = spannableString // TextView에 SpannableString을 설정하여 밑줄이 추가된 텍스트를 표시합니다.
+
+        textView.setOnClickListener {
+            moveToWeatherScreen()
+        }
+    }
+
+    // WeatherActivity로 이동
+    private fun moveToWeatherScreen() {
+        val intent = Intent(this, WeatherActivity::class.java)
+        startActivity(intent)
+        finish() // 현재 Activity 종료
     }
 }
