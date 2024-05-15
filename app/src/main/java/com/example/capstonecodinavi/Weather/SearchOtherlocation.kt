392f4e35573428c0518dd781443fa387d0efefa2
로인 <aroyne@naver.com>
2024-05-16 03:44:25 +0900

    add : speechBtn

--- app/src/main/java/com/example/capstonecodinavi/Weather/SearchOtherlocation.kt
+++ app/src/main/java/com/example/capstonecodinavi/Weather/SearchOtherlocation.kt
@@ -1,8 +1,17 @@
 package com.example.capstonecodinavi.Weather
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
@@ -16,9 +25,52 @@ class SearchOtherlocation : AppCompatActivity() {
         setContentView(binding.root)
         setTitle(" ")
         action()
+
+        //  권한 설정
+        requestPermission()
+    }
+
+    // 권한 설정 메소드
+    private fun requestPermission() {
+        // 버전 체크, 권한 허용했는지 체크
+        if (Build.VERSION.SDK_INT >= 23 &&
+            ContextCompat.checkSelfPermission(this@SearchOtherlocation, android.Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this@SearchOtherlocation,
+                arrayOf(android.Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
+    private fun startSpeechRecognition() {
+        // 음성인식 인텐트 생성
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName)    // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")               // 언어 설정
+
+        // 음성인식 실행
+        try {
+            startActivityForResult(intent, 123)
+        } catch (e: Exception) {
+            Toast.makeText(this@SearchOtherlocation, "음성인식을 지원하지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123 && resultCode == RESULT_OK && data != null) {
+            val matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val result = matches?.get(0)
+            // 여기서 인식된 음성 결과(result)를 처리하면 됩니다.
+            binding.searchEt.setText(result)
+        }
     }
 
     private fun action() {
+        // <말하기> 버튼 클릭 시 음성인식 시작
+        binding.speechBtn.setOnClickListener {
+            startSpeechRecognition()
+        }
+
         binding.searchBtn.setOnClickListener {
             val intent = Intent(this, OtherlocationActivity::class.java)
             intent.putExtra("address",binding.searchEt.text.toString())
