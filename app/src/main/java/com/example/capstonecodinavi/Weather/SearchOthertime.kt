563b40155ad3529660c2a4ebd4261c0b8e2acacc
로인 <aroyne@naver.com>
2024-06-02 18:58:51 +0900

    refactor: update SearchOthertime code

--- app/src/main/java/com/example/capstonecodinavi/Weather/SearchOthertime.kt
+++ app/src/main/java/com/example/capstonecodinavi/Weather/SearchOthertime.kt
@@ -1,12 +1,44 @@
 package com.example.capstonecodinavi.Weather
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.User.UserActivity
+import com.example.capstonecodinavi.databinding.ActivityOthertimeBinding
 
 class SearchOthertime : AppCompatActivity() {
+    lateinit var binding: ActivityOthertimeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_othertime)
+        binding = ActivityOthertimeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        action()
+        setTitle(" ")
+    }
+
+    private fun action() {
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    // 홈 버튼 클릭 시 MainActivity로 이동
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
