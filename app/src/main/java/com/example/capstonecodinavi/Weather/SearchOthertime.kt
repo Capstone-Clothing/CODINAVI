756e538359b07722e7c3b607c1431f4d60819129
JMJ <ghtntkdnsk@naver.com>
2024-06-07 00:20:36 +0900

    feat: 현재 날씨 페이지에서 최저/최고 기온 빼고 데이터 연결완료

--- app/src/main/java/com/example/capstonecodinavi/Weather/SearchOthertime.kt
+++ app/src/main/java/com/example/capstonecodinavi/Weather/SearchOthertime.kt
@@ -2,21 +2,49 @@ package com.example.capstonecodinavi.Weather
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivitySearchOthertimeBinding
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import org.json.JSONException
 
 class SearchOthertime : AppCompatActivity() {
+
     lateinit var binding: ActivitySearchOthertimeBinding
+//    var nextNum = intent.getIntExtra("nextNum",0)
+
+
+    companion object {
+        var requestQueue: RequestQueue? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchOthertimeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val lat = intent.getDoubleExtra("lat", 0.0)
+        val lon = intent.getDoubleExtra("lon", 0.0)
+
+        Log.d("checkLatLon","$lat $lon")
+        initData()
         action()
         setTitle(" ")
+        getHourlyWeather(lat, lon)
+    }
+
+    private fun initData() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(applicationContext)
+        }
     }
 
     private fun action() {
@@ -45,5 +73,23 @@ class SearchOthertime : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    fun getHourlyWeather(lat: Double, lon: Double) {
+        val url = "http://3.34.34.170:8080/weather?lat=${lat}&lon=${lon}"
+        val request = object :
+            StringRequest(
+                Method.GET,
+                url,
+                Response.Listener { response ->
+                    try {
+                        Log.d("checkResponse2","$response")
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                Response.ErrorListener { }
+            ) { }
+        request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
+        WeatherActivity.requestQueue!!.add(request)
     }
 }
--- app/src/main/java/com/example/capstonecodinavi/Weather/WeatherActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Weather/WeatherActivity.kt
@@ -1,18 +1,17 @@
 package com.example.capstonecodinavi.Weather
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.capstonecodinavi.databinding.ActivityWeatherBinding
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -21,13 +20,15 @@ import com.android.volley.toolbox.Volley
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
-import com.google.android.gms.location.*
+import com.example.capstonecodinavi.databinding.ActivityWeatherBinding
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.ArrayList
 import java.util.Locale
 
 class WeatherActivity : AppCompatActivity() {
@@ -41,13 +42,11 @@ class WeatherActivity : AppCompatActivity() {
     private var thoroughfare: String? = null
     private var timeInterval: Long = 3
 
-    lateinit var time: String
-
+    var nextNum: Int = 0
     val nowTime = LocalDateTime.now();
     val formatedNowTime = nowTime.format(DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss"))
     val substringNowDate = formatedNowTime.substring(0 until 8)
     val substringNowTime = formatedNowTime.substring(9 until 11)
-    val substringNowTimeToInt = substringNowTime.toInt()
 
     private var weatherList: ArrayList<JSONObject> = ArrayList()
 
@@ -81,6 +80,9 @@ class WeatherActivity : AppCompatActivity() {
 
         binding.hourlyWeatherBtn.setOnClickListener {
             val intent = Intent(this, SearchOthertime::class.java)
+            intent.putExtra("nextNum", nextNum)
+            intent.putExtra("lat", lat)
+            intent.putExtra("lon", lon)
             startActivity(intent)
         }
 
@@ -89,19 +91,21 @@ class WeatherActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        binding.menuBottomNav.setOnItemSelectedListener { menuItem->
-            when(menuItem.itemId) {
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.menu_home -> {
                     // 홈 버튼 클릭 시 MainActivity로 이동
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     true
                 }
+
                 R.id.menu_user -> {
                     val intent = Intent(this, UserActivity::class.java)
                     startActivity(intent)
                     true
                 }
+
                 else -> false
             }
         }
@@ -111,8 +115,15 @@ class WeatherActivity : AppCompatActivity() {
         if (!checkPermissionForLocation(this)) return
         val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             return
         }
 
@@ -173,7 +184,8 @@ class WeatherActivity : AppCompatActivity() {
                     }
                     addresses?.let {
                         if (locality != null && adminArea != null && thoroughfare != null) {
-                            binding.currentLocationTv.text = "${adminArea} ${locality} ${thoroughfare}"
+                            binding.currentLocationTv.text =
+                                "${adminArea} ${locality} ${thoroughfare}"
                         } else if (locality == null) {
                             binding.currentLocationTv.text = "${adminArea} ${thoroughfare}"
                         } else if (adminArea == null) {
@@ -204,6 +216,9 @@ class WeatherActivity : AppCompatActivity() {
                         Log.d("checkResponse", "$response")
                         var weatherStr: String
                         var weatherIconId: Int? = null
+                        lateinit var temp: String
+                        //lateinit var highTemp: String
+                        //lateinit var lowTemp: String
                         lateinit var weather: String
                         lateinit var weather2: String
 
@@ -215,28 +230,39 @@ class WeatherActivity : AppCompatActivity() {
                             val date = item.getString("date")
 
                             if (date.equals(substringNowDate)) {
-
                                 weatherList.add(item.getJSONObject("info"))
-
                             }
                         }
-//                        weather = item.getJSONObject("info").getString("weather")
-//                        weather2 = item.getJSONObject("info").getString("precipitationType")
-
-                        weatherIconId = when {
-                            weather.contains("흐림") -> R.drawable.cloudy
-                            weather.contains("구름많음") -> R.drawable.cloudy
-                            weather.contains("맑음") -> R.drawable.sunny
-                            weather2.contains("비") -> R.drawable.rainy
-                            weather2.contains("눈") -> R.drawable.snowy
-                            weather2.contains("비 또는 눈") -> R.drawable.rainy
-                            else -> R.drawable.rainy
-                        }
-
-                        Log.d("checkWeather = ", weather)
-                        Log.d("checkWeather2 = ", weather2)
                         Log.d("weatherList = ", "$weatherList")
 
+
+                        for (i in 0 until weatherList.size) {
+                            if (weatherList.get(i).getString("time")
+                                    .equals(substringNowTime + "00")
+                            ) {
+                                weather = weatherList.get(i).getString("weather")
+                                weather2 = weatherList.get(i).getString("precipitationType")
+                                temp = weatherList.get(i).getString("temp")
+                            }
+
+                            nextNum = i + 1
+                        }
+
+                        if (weather.contains("흐림")) {
+                            weatherIconId = R.drawable.cloudy
+                        } else if (weather.contains("구름많음")) {
+                            weatherIconId = R.drawable.cloudy
+                        } else if (weather.contains("맑음")) {
+                            weatherIconId = R.drawable.sunny
+                        } else if (weather2.contains("비")) {
+                            weatherIconId = R.drawable.rainy
+                        } else if (weather2.contains("눈")) {
+                            weatherIconId = R.drawable.snowy
+                        } else if (weather2.contains("비 또는 눈")) {
+                            weatherIconId = R.drawable.rainy
+                        } else {
+                            weatherIconId = R.drawable.rainy
+                        }
 
                         if (weather.contains("흐림")) {
                             weatherStr = "흐림"
@@ -257,15 +283,16 @@ class WeatherActivity : AppCompatActivity() {
                         }
 
                         binding.weatherIV.setImageResource(weatherIconId!!)
-                        binding.currentWeatherTv.text = "날씨 : ${weatherStr}"
-//                        binding.currentWeatherTv2.text = "기온 : ${temp}º"
-//                        recommendCodi(temp.toDouble())
+                        binding.currentWeatherTv.text = "날씨 : $weatherStr"
+                        binding.temperatureTv.text = "기온 : ${temp}º"
+//                        binding.highLowTempTv.text = "최고 : ${}"
+                        recommendCodi(temp.toDouble())
                     } catch (e: JSONException) {
                         e.printStackTrace()
                     }
                 },
                 Response.ErrorListener { }
-            ) { }
+            ) {}
         request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
         requestQueue!!.add(request)
     }
@@ -280,7 +307,8 @@ class WeatherActivity : AppCompatActivity() {
 
             } catch (e: JSONException) {
                 e.printStackTrace()
-            } },
+            }
+        },
             Response.ErrorListener { }) {}
         request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
         requestQueue!!.add(request)
@@ -307,7 +335,10 @@ class WeatherActivity : AppCompatActivity() {
                 return true
             } else {
                 // 권한이 없으므로 권한 요청 알림 보내기
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSION_LOCATION
                 )
                 return false
             }
