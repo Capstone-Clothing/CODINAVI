8f722987f13e6a451011000e5c1429b0dbc17f0f
윤진 <151013932+maruru301@users.noreply.github.com>
2024-06-08 16:28:50 +0900

    style: Add Occasion Activity #22

---
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/OccasionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonecodinavi.Recommend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonecodinavi.R
+
+class OccasionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_occasion)
+    }
+}
