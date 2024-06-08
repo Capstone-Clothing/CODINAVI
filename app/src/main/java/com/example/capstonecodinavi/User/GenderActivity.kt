9460ab6e8f51d29acbe6bf1ff5719f59296736bb
윤진 <151013932+maruru301@users.noreply.github.com>
2024-06-08 16:08:07 +0900

    feat: gender radioBtn check

--- app/src/main/java/com/example/capstonecodinavi/User/GenderActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/User/GenderActivity.kt
@@ -1,18 +1,36 @@
 package com.example.capstonecodinavi.User
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.databinding.ActivityGenderBinding
 
 class GenderActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGenderBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGenderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 초기화
+        sharedPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE)
+
+        val gender = sharedPreferences.getInt("gender", -1)
+
+        Log.d("gender", gender.toString())
+
+        if (gender!=-1) {
+            val btn = binding.radioGroup.getChildAt(gender) as? RadioButton
+            btn?.isChecked = true
+        }
+
         setTitle(" ")
         action()
     }
@@ -23,7 +41,10 @@ class GenderActivity : AppCompatActivity() {
         }
 
         binding.genderChangeBtn.setOnClickListener {
-
+            val index: Int = binding.radioGroup.indexOfChild(findViewById(binding.radioGroup.checkedRadioButtonId))
+            saveGender(index)
+            Toast.makeText(this, "...", Toast.LENGTH_SHORT).show()
+            finish()
         }
 
         binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
@@ -41,5 +62,11 @@ class GenderActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    private fun saveGender(gender: Int) {
+        val editor = sharedPreferences.edit()
+        editor.putInt("gender", gender)
+        editor.apply()
     }
 }
