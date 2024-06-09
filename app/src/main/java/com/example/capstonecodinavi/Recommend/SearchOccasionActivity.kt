827ecd50bae60059290fde031d86a6558d4efce9
로인 <aroyne@naver.com>
2024-06-09 17:00:05 +0900

    style: add search_occasion

---
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/SearchOccasionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonecodinavi.Recommend
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonecodinavi.R
+
+class SearchOccasionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_occasion)
+    }
+}
