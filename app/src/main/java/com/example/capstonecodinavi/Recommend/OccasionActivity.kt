2e7d6869662b7222954700d4d52203c00d4e6ccf
로인 <aroyne@naver.com>
2024-06-09 17:45:19 +0900

    style: OccasionActivity 수정 #16

--- app/src/main/java/com/example/capstonecodinavi/Recommend/OccasionActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/OccasionActivity.kt
@@ -1,12 +1,42 @@
 package com.example.capstonecodinavi.Recommend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.User.UserActivity
+import com.example.capstonecodinavi.databinding.ActivityOccasionBinding
 
 class OccasionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOccasionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_occasion)
+        binding = ActivityOccasionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle(" ")
+        action()
+    }
+
+    private fun action() {
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
