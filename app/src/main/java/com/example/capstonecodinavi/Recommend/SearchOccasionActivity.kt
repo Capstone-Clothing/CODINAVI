825e170c27fb3e1592ec85e51b276d0a7b4ddea4
로인 <aroyne@naver.com>
2024-06-09 17:40:09 +0900

    style: SearchOccasionActivity ëy 버ì튼 연결 #16

--- app/src/main/java/com/example/capstonecodinavi/Recommend/SearchOccasionActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/SearchOccasionActivity.kt
@@ -1,12 +1,48 @@
 package com.example.capstonecodinavi.Recommend
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.capstonecodinavi.Guide.IntroduceAppBtn
+import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.User.UserActivity
+import com.example.capstonecodinavi.databinding.ActivitySearchOccasionBinding
 
 class SearchOccasionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchOccasionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_occasion)
+        binding = ActivitySearchOccasionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle(" ")
+        action()
+    }
+
+    private fun action() {
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.occasionBtn.setOnClickListener {
+            val intent = Intent( this, OccasionActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
