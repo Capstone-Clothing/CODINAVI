1f756978f77e9d7831de20cb37288b36c59f7f8f
JMJ <ghtntkdnsk@naver.com>
2024-06-15 19:47:32 +0900

    OthertimeActivity 서버와 API 통신 및 Activity에 데이터 연결 완료

--- app/src/main/java/com/example/capstonecodinavi/Weather/OthertimeActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Weather/OthertimeActivity.kt
@@ -3,23 +3,48 @@ package com.example.capstonecodinavi.Weather
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivityOthertimeBinding
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import org.json.JSONException
+import org.json.JSONObject
 
 class OthertimeActivity : AppCompatActivity() {
     lateinit var binding: ActivityOthertimeBinding
+    lateinit var weather: Weather
+
+    companion object {
+        var requestQueue: RequestQueue? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOthertimeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        weather = intent.getSerializableExtra("weather") as Weather
+
+        initData()
         action()
+        connectData(weather)
+        recommendCodi(weather.temp.toDouble(), "여자")
         setTitle(" ")
+    }
+
+    private fun initData() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(applicationContext)
+        }
     }
 
     private fun action() {
@@ -43,5 +68,39 @@ class OthertimeActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    fun connectData(weather: Weather) {
+        binding.timeTv.text = "시간 : ${weather.time.substring(0 until 2)}시"
+        binding.humidityTv.text = "습도 : ${weather.humidity}%"
+        binding.weatherIV.setImageResource(weather.weatherIconId)
+        binding.precipitationTv.text = "강수확률 : ${weather.precipitation}%"
+        binding.precipitationProbTv.text = "강수량 : ${weather.precipitationProbability}"
+        binding.temperatureTv.text = "기온 : ${weather.temp}º"
+        binding.currentWeatherTv.text = "날씨 : ${weather.weatherText}"
+    }
+
+    private fun recommendCodi(temp: Double, gender: String) {
+        val url = "http://3.34.34.170:8080/weather/clothInfo?temp=${temp}&gender=${gender}"
+        Log.d("checkURL2","$url")
+        val request = object :
+            StringRequest(
+                Method.GET,
+                url,
+                Response.Listener { response ->
+                    try {
+                        val jsonObject = JSONObject(response)
+                        val codi = jsonObject.getString("codi")
+                        val clothRec = jsonObject.getString("clothRec")
+                        binding.recommendClothTv.text = codi
+                        binding.recommendItemTv.text = clothRec
+
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                },
+                Response.ErrorListener { }) {}
+        request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
+        requestQueue!!.add(request)
     }
 }
