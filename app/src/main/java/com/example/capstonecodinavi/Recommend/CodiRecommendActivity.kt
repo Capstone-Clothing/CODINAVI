07cf21e487c858a5bbb7006a7685d28708b91896
JMJ <ghtntkdnsk@naver.com>
2024-06-20 04:51:21 +0900

    feat: 코디 추천 및 색상 추천 기능 완료

--- app/src/main/java/com/example/capstonecodinavi/Recommend/CodiRecommendActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/CodiRecommendActivity.kt
@@ -3,10 +3,19 @@ package com.example.capstonecodinavi.Recommend
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.capstonecodinavi.Camera.AnalysisRequest
+import com.example.capstonecodinavi.Camera.AnalysisResult
+import com.example.capstonecodinavi.Camera.CameraActivity
+import com.example.capstonecodinavi.Camera.RetrofitFlaskClient
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivityCodiBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.UUID
 
 class CodiRecommendActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCodiBinding
@@ -14,6 +23,33 @@ class CodiRecommendActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCodiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val imageId = intent.getStringExtra("imageId")!!
+        val request = AnalysisRequest(
+            bucket_name = "codinavi-image",
+            image_key = imageId
+        )
+
+        val call = RetrofitFlaskClient.instance.getAnalysisResult(request)
+        call.enqueue(object : Callback<AnalysisResult> {
+            override fun onResponse(call: Call<AnalysisResult>, response: Response<AnalysisResult>) {
+                Log.d("CameraFragment", "Response received: ${response.body()}")
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    result?.let {
+                        val clothingItem = it.result[0]
+                        val message2 = "${clothingItem.어울리는상의or하의추천}을 추천드립니다."
+                        binding.codiTv.text = message2
+                    }
+                } else {
+                    Log.e("CameraFragment", "Failed to get analysis result: ${response.errorBody()?.string()}")
+                }
+            }
+
+            override fun onFailure(call: Call<AnalysisResult>, t: Throwable) {
+                Log.e("CameraFragment", "Error: ${t.message}")
+            }
+        })
         setTitle(" ")
         action()
     }
--- app/src/main/java/com/example/capstonecodinavi/Recommend/ColorRecommendActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/Recommend/ColorRecommendActivity.kt
@@ -3,20 +3,25 @@ package com.example.capstonecodinavi.Recommend
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.capstonecodinavi.Camera.AnalysisRequest
+import com.example.capstonecodinavi.Camera.AnalysisResult
+import com.example.capstonecodinavi.Camera.RetrofitFlaskClient
 import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
 import com.example.capstonecodinavi.User.UserActivity
 import com.example.capstonecodinavi.databinding.ActivityColorBinding
 import org.json.JSONException
 import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
 
 class ColorRecommendActivity : AppCompatActivity() {
     private lateinit var binding: ActivityColorBinding
-    private lateinit var color: String
 
     companion object {
         var requestQueue: RequestQueue? = null
@@ -26,10 +31,38 @@ class ColorRecommendActivity : AppCompatActivity() {
         binding = ActivityColorBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setTitle(" ")
+
+        var color = ""
+
+        val imageId = intent.getStringExtra("imageId")!!
+        val request = AnalysisRequest(
+            bucket_name = "codinavi-image",
+            image_key = imageId
+        )
+
+        val call = RetrofitFlaskClient.instance.getAnalysisResult(request)
+        call.enqueue(object : Callback<AnalysisResult> {
+            override fun onResponse(call: Call<AnalysisResult>, response: retrofit2.Response<AnalysisResult>) {
+                Log.d("CameraFragment", "Response received: ${response.body()}")
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    result?.let {
+                        val clothingItem = it.result[0]
+                        color = clothingItem.색상
+                        recommendColor(color)
+                    }
+                } else {
+                    Log.e("CameraFragment", "Failed to get analysis result: ${response.errorBody()?.string()}")
+                }
+            }
+
+            override fun onFailure(call: Call<AnalysisResult>, t: Throwable) {
+                Log.e("CameraFragment", "Error: ${t.message}")
+            }
+        })
+
         initData()
         action()
-        color = "파란색"
-        //recommendColor(color)
     }
     private fun initData() {
         if (requestQueue == null) {
@@ -65,21 +98,24 @@ class ColorRecommendActivity : AppCompatActivity() {
             }
         }
     }
-//    private fun recommendColor(color: String) {
-//        val url = "http://3.34.34.170:8080/color/recommend?color=${color}"
-//        val request = object : StringRequest(Method.GET, url, Response.Listener { response ->
-//            try {
-//                val jsonObject = JSONObject(response)
-//                val colorInfo = jsonObject.getString("matchColor")
-//                binding.colorTv.text = colorInfo
-//
-//            } catch (e: JSONException) {
-//                e.printStackTrace()
-//            }
-//
-//        }, Response.ErrorListener { }) {}
-//        request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
-//        requestQueue!!.add(request)
-//    }
+    private fun recommendColor(color: String) {
+        val url = "http://3.34.34.170:8080/color/recommend?color=${color}"
+        val request = object : StringRequest(Method.GET, url, Response.Listener { response ->
+            try {
+                val jsonObject = JSONObject(response)
+                var colorInfo = jsonObject.getString("matchColor")
+                if (colorInfo.equals("모든 색상과 잘 어울립니다.")) {
+                    binding.colorTv.text = "${colorInfo}"
+                } else {
+                    binding.colorTv.text = "${colorInfo}를 추천드립니다."
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+        }, Response.ErrorListener { }) {}
+        request.setShouldCache(false) // 이전 결과가 있어도 새 요청하여 결과 보여주기
+        requestQueue!!.add(request)
+    }
 }
 
