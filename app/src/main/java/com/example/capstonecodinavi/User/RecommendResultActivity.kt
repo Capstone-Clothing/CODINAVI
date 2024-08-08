ecee17ce05a22c94ef9129c9bb5ad81cd6583f64
로인 <151013952+bboroin@users.noreply.github.com>
2024-08-08 19:11:04 +0900

    RecommendResultActivity 코드 작성 #63

--- app/src/main/java/com/example/capstonecodinavi/User/RecommendResultActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/User/RecommendResultActivity.kt
@@ -1,12 +1,41 @@
 package com.example.capstonecodinavi.User
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.databinding.ActivityRecommendResultBinding
 
 class RecommendResultActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRecommendResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recommend_result)
+        binding = ActivityRecommendResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle(" ")
+        action()
+    }
+
+    private fun action() {
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
