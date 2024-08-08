dcf09b8c56df391b9cee2156e6bdeaf58518bdd5
로인 <151013952+bboroin@users.noreply.github.com>
2024-08-08 19:06:39 +0900

    ResultActivity.kt 코드 작성 #63

--- app/src/main/java/com/example/capstonecodinavi/User/ResultListActivity.kt
+++ app/src/main/java/com/example/capstonecodinavi/User/ResultListActivity.kt
@@ -1,12 +1,47 @@
 package com.example.capstonecodinavi.User
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstonecodinavi.Main.MainActivity
 import com.example.capstonecodinavi.R
+import com.example.capstonecodinavi.databinding.ActivityResultListBinding
 
 class ResultListActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResultListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result_list)
+        binding = ActivityResultListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setTitle(" ")
+        action()
+    }
+
+    private fun action() {
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        // 임시로 result1 만 넣어둠
+        binding.result1.setOnClickListener {
+            val intent = Intent(this, RecommendResultActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.menuBottomNav.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_user -> {
+                    val intent = Intent(this, UserActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
